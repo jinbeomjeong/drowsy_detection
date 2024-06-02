9d2abd3c023a83e4635d0a75f129b48d4706fa74
jinbeom, jeong <ppgt1225@naver.com>
2024-06-03 04:20:43 +0900

    fix datamodule for ai hub dataset

---
+++ utils/eye_det_model.py
@@ -0,0 +1,83 @@
+import sys, torch, torchmetrics
+import pytorch_lightning as pl
+from torchvision.models import mobilenet_v2
+from pytorch_lightning.callbacks import TQDMProgressBar
+
+
+class EyeDetModel(pl.LightningModule):
+    def __init__(self, learning_rate: float = 0.001):
+        super().__init__()
+        self.__learning_rate = learning_rate
+        self.__model = mobilenet_v2(weights=None)
+        self.__model.classifier[1] = torch.nn.Linear(self.__model.classifier[1].in_features, out_features=2)
+
+        self.__criterion = torch.nn.CrossEntropyLoss()
+        self.__accuracy_score = torchmetrics.Accuracy(task='binary')
+        self.__precision_score = torchmetrics.Precision(task='binary')
+        self.__recall_score = torchmetrics.Recall(task='binary')
+        self.__f1_score = torchmetrics.F1Score(task='binary')
+
+    def forward(self, x):
+        return self.__model(x)
+
+    def training_step(self, batch, batch_idx):
+        img, label = batch
+        label = label
+        pred = self.forward(img)
+        loss = self.__criterion(pred, label)
+
+        pred_result = torch.argmax(pred, dim=1)
+
+        self.log('train_loss', loss, sync_dist=True, on_step=True, on_epoch=False, prog_bar=True)
+        self.log(name='train_accuracy', value=self.__accuracy_score(pred_result, label), sync_dist=True, on_step=True, on_epoch=False, prog_bar=True)
+        self.log(name='train_precision', value=self.__precision_score(pred_result, label), sync_dist=True, on_step=True, on_epoch=False, prog_bar=True)
+        self.log(name='train_recall', value=self.__recall_score(pred_result, label), sync_dist=True, on_step=True, on_epoch=False, prog_bar=True)
+        self.log(name='train_f1', value=self.__f1_score(pred_result, label), sync_dist=True, on_step=True, on_epoch=False, prog_bar=True)
+
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        img, label = batch
+        label = label
+        pred = self.forward(img)
+        loss = self.__criterion(pred, label)
+
+        pred_result = torch.argmax(pred, dim=1)
+
+        self.log(name='val_loss', value=loss, sync_dist=True, on_step=False, on_epoch=True, prog_bar=True)
+        self.log(name='val_accuracy', value=self.__accuracy_score(pred_result, label), sync_dist=True, on_step=False, on_epoch=True, prog_bar=True)
+        self.log(name='val_precision', value=self.__precision_score(pred_result, label), sync_dist=True, on_step=False, on_epoch=True, prog_bar=True)
+        self.log(name='val_recall', value=self.__recall_score(pred_result, label), sync_dist=True, on_step=False, on_epoch=True, prog_bar=True)
+        self.log(name='val_f1', value=self.__f1_score(pred_result, label), sync_dist=True, on_step=False, on_epoch=True, prog_bar=True)
+
+        return loss
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.__model.parameters(), lr=self.__learning_rate)
+
+        return optimizer
+
+
+class LitProgressBar(TQDMProgressBar):
+    def init_validation_tqdm(self):
+        bar = super().init_validation_tqdm()
+
+        if not sys.stdout.isatty():
+            bar.disable = True
+
+        return bar
+
+    def init_predict_tqdm(self):
+        bar = super().init_predict_tqdm()
+
+        if not sys.stdout.isatty():
+            bar.disable = True
+
+        return bar
+
+    def init_test_tqdm(self):
+        bar = super().init_test_tqdm()
+
+        if not sys.stdout.isatty():
+            bar.disable = True
+        return bar
---
+++ utils/mrl_eye_module.py
@@ -0,0 +1,67 @@
+import os, cv2
+import pytorch_lightning as pl
+from torch.utils.data import DataLoader, random_split, Dataset
+from torchvision import transforms
+
+
+class MrleyesDataset(Dataset):
+    def __init__(self, img_path_list: list, label_list: list, transform: transforms):
+        self.__img_path_list = img_path_list
+        self.__label_list = label_list
+        self.__transform = transform
+
+    def __len__(self):
+        return len(self.__img_path_list)
+
+    def __getitem__(self, idx):
+        eye_img = cv2.imread(self.__img_path_list[idx])
+        eye_img = self.__transform(eye_img)
+
+        return eye_img, self.__label_list[idx]
+
+
+class MrlEyeDataModule(pl.LightningDataModule):
+    def __init__(self, dataset_path: str, batch_size: int = 16, n_workers: int = 8):
+        super().__init__()
+        self.__h_offset_scale: float = 0.08
+        self.__v_offset_scale: float = 0.08
+        self.__eye_img_files = list('')
+        self.__eye_label_list = list('')
+
+        self.__dataset_path = dataset_path
+        self.batch_size = batch_size
+        self.num_workers = n_workers
+        self.__transform = transforms.Compose([transforms.ToPILImage(),
+                                               transforms.Resize((64, 64)),
+                                               transforms.ToTensor(),
+                                               transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+
+    def prepare_data(self):
+        for (path, dirs, files) in os.walk(self.__dataset_path):
+            for file in files:
+                if os.path.splitext(file)[1] == '.png':
+                    self.__eye_img_files.append(path + os.path.sep + file)
+
+                    for i in range(4):
+                        sep_idx = file.find('_')
+                        file = file[sep_idx + 1:]
+
+                    self.__eye_label_list.append(int(file[0]))
+
+    def setup(self, stage=None):
+        self.__train_dataset = MrleyesDataset(img_path_list=self.__eye_img_files, label_list=self.__eye_label_list,
+                                              transform=self.__transform)
+        self.__train_dataset, self.__val_dataset = random_split(self.__train_dataset, lengths=[0.6, 0.4])
+        self.__val_dataset, self.__test_dataset = random_split(self.__train_dataset, lengths=[0.5, 0.5])
+
+    def train_dataloader(self):
+        return DataLoader(self.__train_dataset, batch_size=self.batch_size, shuffle=True, num_workers=self.num_workers,
+                          persistent_workers=True)
+
+    def val_dataloader(self):
+        return DataLoader(self.__val_dataset, batch_size=self.batch_size, shuffle=False, num_workers=self.num_workers,
+                          persistent_workers=True)
+
+    def test_dataloader(self):
+        return DataLoader(self.__test_dataset, batch_size=self.batch_size, shuffle=False, num_workers=self.num_workers,
+                          persistent_workers=True)
